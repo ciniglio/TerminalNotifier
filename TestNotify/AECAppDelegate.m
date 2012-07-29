ba9089b8047a8c9d092b3b2e6ef24a74b2f24f8b
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-07-29 00:19:20 -0700

    does some cleanup on exit and kills self if appropriate.

--- TestNotify/AECAppDelegate.m
+++ TestNotify/AECAppDelegate.m
@@ -7,37 +7,85 @@
 //
 
 #import "AECAppDelegate.h"
+#import "AECArgumentParser.h"
 
 @implementation AECAppDelegate
 
+void cleaner()
+{
+    [[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
+    [[NSApplication sharedApplication] terminate:NSApp];
+}
+
+- (void)handleActiveAppNotification:(NSNotification *)notification
+{
+    NSLog(@"Got notified: %@", notification);
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    [NSApp setApplicationIconImage:[NSImage imageNamed:NSImageNameComputer]];
-//    NSUserNotification *n = [[NSUserNotification alloc] init];
-//    [n setHasActionButton:YES];
-//    [n setActionButtonTitle:@"WHOA"];
-//    [n setTitle:@"Alejandro"];
-//    [n setSubtitle:@"Testing"];
     NSUserNotification *notification = [self createUserNotificationFromArguments];
-
-    [[NSUserNotificationCenter defaultUserNotificationCenter]deliverNotification:notification];
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+    
     [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self selector:@selector(handleActiveAppNotification) name:@"NSWorkspaceDidDeactivateApplicationNotification" object:nil];
+    
+    signal(SIGINT, &cleaner);
+}
+
+- (void)applicationDidBecomeActive:(NSNotification *)notification
+{
+    NSUserNotification *user_notification = [self createUserNotificationFromArguments];
+    [[NSUserNotificationCenter defaultUserNotificationCenter]deliverNotification:user_notification];
 }
 
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center didActivateNotification:(NSUserNotification *)notification
 {
     [[NSWorkspace sharedWorkspace] launchApplication:@"/Applications/Utilities/Terminal.app"];
     [center removeDeliveredNotification:notification];
+    [[NSApplication sharedApplication] terminate:self];
+}
+
+- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
+{
+    [[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
+    return NSTerminateNow;
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    [[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
+}
+
+- (void)setSoundForNotification:(NSUserNotification *)notification
+{
+    NSString * title = [notification title];
+    if ([title rangeOfString:@"fail" options:NSCaseInsensitiveSearch].location != NSNotFound)
+    {
+        [notification setSoundName:@"Ping"];
+    }
+    else if ([title rangeOfString:@"warn" options:NSCaseInsensitiveSearch].location != NSNotFound)
+    {
+        [notification setSoundName:@"Ping"];
+    }
+    else {
+        [notification setSoundName:@"Glass"];
+    }
+    
 }
 
 - (NSUserNotification *)createUserNotificationFromArguments
 {
     NSUserNotification *notification = [[NSUserNotification alloc] init];
     NSArray *arguments = [[NSProcessInfo processInfo] arguments];
-    NSString *title = [arguments objectAtIndex:0] == nil ? [arguments objectAtIndex:0] : @"Ruby Notifier";
+    NSString *title = [arguments objectAtIndex:1] != nil ? [arguments objectAtIndex:1] : @"Terminal";
     [notification setTitle:title];
-    NSString *subtitle = [arguments objectAtIndex:1] == nil ? [arguments objectAtIndex:1] : @"Ruby Notifier";
+    NSString *subtitle = [arguments objectAtIndex:2] != nil ? [arguments objectAtIndex:2] : @"Done!";
     [notification setSubtitle:subtitle];
+    NSString *info_text = [arguments objectAtIndex:3] != nil ? [arguments objectAtIndex:3] : @"Your long-running process has finished";
+    [notification setInformativeText:info_text];
+    [self setSoundForNotification:notification];
+
     
     return notification;
 }
